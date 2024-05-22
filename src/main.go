2fefc2657629d42bafbb5eacba5b173317265f47
Victor <victor.mendele68@gmail.com>
2024-05-22 09:33:49 +0200

    remove gin from the program

--- src/main.go
+++ src/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"context"
-	"net/http"
+	"fmt"
+	"html/template"
 	"os"
 	"strings"
 
 	"log"
 
 	"math/rand"
+	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -51,63 +52,146 @@ func main() {
 
 	collection := client.Database("url_shortener").Collection("url_mapping")
 
-	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
-			"shortenUrl": nil,
-		})
+	name, err := collection.Indexes().CreateOne(context.TODO(), mongo.IndexModel{
+		Keys:    bson.D{{Key: "shortenurl", Value: 1}},
+		Options: options.Index().SetUnique(true).SetExpireAfterSeconds(60),
 	})
-	r.POST("/", func(c *gin.Context) {
-		var form Form
 
-		c.Bind(&form)
+	if err != nil {
+		log.Fatalln("Cannot create indexes on the MongoDB collection", err)
+	}
 
-		if form.URL == "" {
-			c.Status(400)
+	fmt.Printf("Index successfully created with name '%s'\n", name)
+
+	indexTemplate := template.Must(template.ParseFiles("templates/index.tmpl.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			err := indexTemplate.Execute(w, make(map[string]interface{}))
+
+			if err != nil {
+				fmt.Fprint(w, "Something went wrong when rendering the template")
+			}
+			return
+		case http.MethodPost:
+			urlToShorten := r.FormValue("url")
+
+			if urlToShorten == "" {
+				w.WriteHeader(http.StatusBadRequest)
+				fmt.Fprint(w, "You must provide the 'url' form value")
+				return
+			}
+
+			urlMapping := UrlMappingDocument{
+				ShortenUrl:  RandomId(5),
+				CompleteUrl: urlToShorten,
+			}
+
+			_, err := collection.InsertOne(r.Context(), urlMapping)
+
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprint(w, "Cannot insert in the DB")
+				return
+			}
+
+			w.WriteHeader(http.StatusCreated)
+			indexTemplate.Execute(w, urlMapping)
+			return
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprint(w, "Method not supported")
 			return
 		}
-
-		mapping := UrlMappingDocument{
-			ShortenUrl:  RandomId(6),
-			CompleteUrl: form.URL,
-		}
-
-		_, err := collection.InsertOne(context.TODO(), mapping)
-
-		if err != nil {
-			c.Status(500)
-			return
-		}
-
-		c.HTML(http.StatusCreated, "index.tmpl.html", gin.H{
-			"shortenUrl": BuildShortenUrlFromId(mapping.ShortenUrl),
-		})
 	})
-	r.GET("/r/:id", func(c *gin.Context) {
-		id := c.Param("id")
+
+	http.HandleFunc("GET /r/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
 
 		if id == "" {
-			c.Status(400)
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, "Unknown shorten URL")
+			return
 		}
 
 		var mapping UrlMappingDocument
-		err := collection.FindOne(context.TODO(), bson.D{{"shortenurl", id}}).Decode(&mapping)
+
+		err := collection.FindOne(r.Context(), bson.D{{Key: "shortenurl", Value: id}}).Decode(&mapping)
 
 		if err == mongo.ErrNoDocuments {
-			c.Status(http.StatusNotFound)
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "Shorten URL not found in DB")
 			return
 		}
 
-		if err != nil {
-			c.Status(http.StatusInternalServerError)
-			return
-		}
-
-		c.Redirect(http.StatusTemporaryRedirect, mapping.CompleteUrl)
-
+		w.Header().Add("Location", mapping.CompleteUrl)
+		w.WriteHeader(http.StatusTemporaryRedirect)
 	})
-	log.Fatal(r.Run())
+
+	server := &http.Server{
+		Addr: ":4500",
+	}
+
+	log.Fatal(server.ListenAndServe())
+
+	// r := gin.Default()
+	// r.LoadHTMLGlob("templates/*")
+	// r.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+	// 		"shortenUrl": nil,
+	// 	})
+	// })
+	// r.POST("/", func(c *gin.Context) {
+	// 	var form Form
+
+	// 	c.Bind(&form)
+
+	// 	if form.URL == "" {
+	// 		c.Status(400)
+	// 		return
+	// 	}
+
+	// 	mapping := UrlMappingDocument{
+	// 		ShortenUrl:  RandomId(6),
+	// 		CompleteUrl: form.URL,
+	// 	}
+
+	// 	_, err := collection.InsertOne(context.TODO(), mapping)
+
+	// 	if err != nil {
+	// 		c.Status(500)
+	// 		return
+	// 	}
+
+	// 	c.HTML(http.StatusCreated, "index.tmpl.html", gin.H{
+	// 		"shortenUrl": BuildShortenUrlFromId(mapping.ShortenUrl),
+	// 	})
+	// })
+	// r.GET("/r/:id", func(c *gin.Context) {
+	// 	id := c.Param("id")
+
+	// 	if id == "" {
+	// 		c.Status(400)
+	// 	}
+
+	// 	var mapping UrlMappingDocument
+	// 	err := collection.FindOne(context.TODO(), bson.D{{"shortenurl", id}}).Decode(&mapping)
+
+	// 	if err == mongo.ErrNoDocuments {
+	// 		c.Status(http.StatusNotFound)
+	// 		return
+	// 	}
+
+	// 	if err != nil {
+	// 		c.Status(http.StatusInternalServerError)
+	// 		return
+	// 	}
+
+	// 	c.Redirect(http.StatusTemporaryRedirect, mapping.CompleteUrl)
+
+	// })
+	// log.Fatal(r.Run())
 }
 
 var letters = [...]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
